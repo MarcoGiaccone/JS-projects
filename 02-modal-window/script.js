34d9bfeb0bd2701ff267ea39ddb747b289c21cbb
Marco Giaccone <marco.giaccone@edu.itspiemonte.it>
2024-10-31 10:57:20 +0100

    un po di refactoring

--- 02-modal-window/script.js
+++ 02-modal-window/script.js
@@ -6,17 +6,21 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 const buttons = document.querySelectorAll('.show-modal');
 
-buttons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    })
-});
+function showModal() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 
-btnCloseModal.addEventListener('click', function () {
+function hideModal() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-})
+}
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', showModal);
+});
+
+btnCloseModal.addEventListener('click', hideModal);
 
 
 
